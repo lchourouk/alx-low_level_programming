09663776a4df28cb137ae096afb87ff9e4c88333
Loubna Chourouk <loubnachourouk@gmail.com>
2023-07-24 18:37:03 -0700

    0x05- C programming - 02 _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string param
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
