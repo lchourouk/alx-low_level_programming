a4da50919ff89322daec9369244ac89a7b2756b3
Loubna Chourouk <loubnachourouk@gmail.com>
2023-07-28 18:31:15 -0700

    101- print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: int param
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
