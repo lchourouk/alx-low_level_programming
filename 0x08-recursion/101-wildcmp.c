4acc3265e6ba64aa0e1b9a4d3781ee17a967521e
Loubna Chourouk <loubnachourouk@gmail.com>
2023-08-04 19:59:37 -0700

    101- wilcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * move_past_star - iterates past asterisk
+ *
+ * @s2: string param
+ *
+ * Return: ptr after asterisk
+ */
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - makes magic a reality
+ *
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: 1 or 0
+ */
+
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - compares 2 strings
+ *
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: 1 or 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
