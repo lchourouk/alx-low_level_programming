16146a7b420157839c51367f1fe1783047d46b4e
Loubna Chourouk <loubnachourouk@gmail.com>
2023-08-04 19:07:22 -0700

    3- factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - calculates the factorial
+ *
+ * @n: int param
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
