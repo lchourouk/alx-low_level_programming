c90b53ab18a86c9895c6cc0571bff7d97725e5e2
Loubna Chourouk <loubnachourouk@gmail.com>
2023-08-14 18:36:48 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ *
+ * @name:string  name
+ * @age: float age
+ * @owner: string owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
