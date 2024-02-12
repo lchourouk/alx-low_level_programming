0540de57ad6b6531fccb739bc22dcdb986f041f9
lchourouk <loubnachourouk@gmail.com>
2024-02-12 13:39:47 -0800

    IO

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and
+ * prints it to the POSIX standard output.
+ *
+ * @filename: ptr to the file to read
+ * @letters: nbr of bytes to read
+ *
+ * Return: nbr of printed bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t nbr_bytes;
+	int fildes;
+	char *buf = malloc(letters * sizeof(char));
+
+	if (!filename)
+		return (0);
+	fildes = open(filename, O_RDONLY);
+	if (fildes == -1)
+		return (0);
+	nbr_bytes = read(fildes, buf, letters);
+	nbr_bytes = write(STDOUT_FILENO, buf, bytes);
+	if (nbr_bytes == -1 || nbr_bytes != letters)
+		return (0);
+	close(fildes);
+	free(buf);
+	return (bytes);
+}
